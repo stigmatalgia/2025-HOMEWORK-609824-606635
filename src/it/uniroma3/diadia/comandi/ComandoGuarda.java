1ab4e86a64f607c948ad8204394b4210110d6db0
Valerio Tatti <valerio.tatti04@outlook.com>
2025-06-05 21:36:53 +0200

    Quick ProjectPath fix + ComandoGuarda fix

--- src/it/uniroma3/diadia/comandi/ComandoGuarda.java
+++ src/it/uniroma3/diadia/comandi/ComandoGuarda.java
@@ -10,7 +10,7 @@ public class ComandoGuarda extends AbstractComando implements Comando{
 
 	@Override
 	public void esegui(Partita partita, IO IO) {
-		IO.mostraMessaggio(partita.getStanzaCorrente().getDescrizione()+"\nCFU rimasti:" + partita.getGiocatore().getCfu() + "");
+		IO.mostraMessaggio(partita.getStanzaCorrente().getDescrizione()+"\nCFU rimasti:" + partita.getGiocatore().getCfu() + "\n"+"nella stanza c'e' un "+partita.getStanzaCorrente().getPersonaggio().getClass().getSimpleName());
 	}
 	
 	@Override
